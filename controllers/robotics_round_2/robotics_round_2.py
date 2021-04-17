df80b7b08f40063eb1963aaddab0b3b40bdb7f1c
DNA5769 <dennisthomas2002@gmail.com>
2021-04-17 20:25:57 +0530

    Fixed bug (it's working!!!)

--- controllers/robotics_round_2/robotics_round_2.py
+++ controllers/robotics_round_2/robotics_round_2.py
@@ -5,8 +5,8 @@ robot = Robot()
 timestep = 32
 max_speed = 6.28
 
-left_motor = robot.getDevice('motor1')
-right_motor = robot.getDevice('motor2')
+right_motor = robot.getDevice('motor1')
+left_motor = robot.getDevice('motor2')
 
 straight_ir = robot.getDevice('ir0')
 left_ir = robot.getDevice('ir1')
@@ -23,26 +23,29 @@ if __name__ == "__main__":
     right_ir.enable(timestep)
     
     while robot.step(timestep) != -1:
-        straight = straight_ir.getValue()>500
-        left = left_ir.getValue()>500
-        right = right_ir.getValue()>500
+        limit = 500
+        straight = straight_ir.getValue()>limit
+        left = left_ir.getValue()>limit
+        right = right_ir.getValue()>limit
         
         if left:
-            left_speed = 0.25*max_speed
-            right_speed = 0.75*max_speed
+            left_speed = 0*max_speed
+            right_speed = 0.5*max_speed
             print("Left")
         elif straight:
             left_speed = 0.5*max_speed
             right_speed = 0.5*max_speed
             print("Straight")
         elif right:
-            left_speed = 0.75*max_speed
-            right_speed = 0.25*max_speed
+            left_speed = 0.5*max_speed
+            right_speed = 0*max_speed
             print("Right")
         else:
-            left_speed = 0
+            left_speed = 0*max_speed
             right_speed = max_speed
             print("Back")
 
+        print(f'Left: {left_ir.getValue()} Straight: {straight_ir.getValue()} Right: {right_ir.getValue()}')
+    
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
