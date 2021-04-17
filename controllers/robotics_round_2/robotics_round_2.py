dbd0b6ed395a847bd3fd7519859bdbcd192e43bb
DNA5769 <dennisthomas2002@gmail.com>
2021-04-17 19:09:32 +0530

    Code cleanup

--- controllers/robotics_round_2/robotics_round_2.py
+++ controllers/robotics_round_2/robotics_round_2.py
@@ -1,44 +1,33 @@
-"""my_first_lfr controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 
+robot = Robot()
+
+timestep = 32
+max_speed = 6.28
+
+left_motor = robot.getDevice('motor1')
+right_motor = robot.getDevice('motor2')
+
+straight_ir = robot.getDevice('ir0')
+left_ir = robot.getDevice('ir1')
+right_ir = robot.getDevice('ir2')
+
 if __name__ == "__main__":
-
-    # create the Robot instance.
-    robot = Robot()
-    
-    # get the time step of the current world.
-    timestep = 32
-    max_speed = 6.28
-    
-    left_motor = robot.getDevice('motor1')
-    right_motor = robot.getDevice('motor2')
     left_motor.setPosition(float('inf'))
-    right_motor.setPosition(float('inf'))
     left_motor.setVelocity(0.0)
+    right_motor.setPosition(float('inf'))
     right_motor.setVelocity(0.0)
-    left_ir = robot.getDistanceSensor('ir1')
+    
+    straight_ir.enable(timestep)
     left_ir.enable(timestep)
-    right_ir = robot.getDistanceSensor('ir2')
     right_ir.enable(timestep)
+    
+    while robot.step(timestep) != -1:
+        straight_ir_value = straight_ir.getValue()
+        left_ir_value = left_ir.getValue()
+        right_ir_value = right_ir.getValue()
+        
+        print("Left: {} Straight: {} Right: {}".format(left_ir_value,straight_ir_value,right_ir_value))
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    left_ir_value = left_ir.getValue()
-    right_ir_value = right_ir.getValue()
-    print("Left: {} right: {}".format(left_ir_value,right_ir_value))
-    left_speed = max_speed*0.25
-    right_speed = max_speed*0.25
-    if(left_ir_value > right_ir_value) and (6<left_ir_value<15):
-        print("Go Left")
-        left_speed = -max_speed*0.25
-    elif(left_ir_value < right_ir_value) and (6<right_ir_value<15):
-        print("Go Right")
-        right_speed = -max_speed*0.25
-    left_motor.setVelocity(left_speed)
-    right_motor.setVelocity(right_speed)
-
-# Enter here exit cleanup code.
+        left_motor.setVelocity(max_speed*0.5)
+        right_motor.setVelocity(max_speed*0.5)
