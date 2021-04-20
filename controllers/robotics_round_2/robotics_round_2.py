1c99a3463dc10d7ab94d86e7c41a4cf461dd39a1
DNA5769 <dennisthomas2002@gmail.com>
2021-04-20 18:41:44 +0530

    Refactored code

--- controllers/robotics_round_2/robotics_round_2.py
+++ controllers/robotics_round_2/robotics_round_2.py
@@ -13,56 +13,73 @@ left_ir = robot.getDevice('ir1')
 right_ir = robot.getDevice('ir2')
 top_ir = robot.getDevice('ir3')
 
-if __name__ == "__main__":
-    left_motor.setPosition(float('inf'))
-    left_motor.setVelocity(0.0)
-    right_motor.setPosition(float('inf'))
-    right_motor.setVelocity(0.0)
-    
-    straight_ir.enable(timestep)
-    left_ir.enable(timestep)
-    right_ir.enable(timestep)
-    top_ir.enable(timestep)
+flag = False
 
-    flag = False
-    c = 25
+counter = 25
+
+limit = 500
+
+state = {
+    'left_speed': 0.0,
+    'right_speed': 0.0
+}
+
+def dispatch(state, action):
+    if action['type'] == 'L':
+        state['left_speed'] = 0.0
+        state['right_speed'] = max_speed
+        print("Left")
+    elif action['type'] == 'S':
+        state['left_speed'] = max_speed
+        state['right_speed'] = max_speed
+        print("Straight")
+    elif action['type'] == 'R':
+        state['left_speed'] = max_speed
+        state['right_speed'] = 0.0
+        print("Right")
+    elif action['type'] == 'B':
+        state['left_speed'] = -max_speed
+        state['right_speed'] = max_speed
+        print("Back")
+    elif action['type'] == 'STOP':
+        state['left_speed'] = 0.0
+        state['right_speed'] = 0.0
+        print("WOOHOO!")
+    else:
+        pass
+
+if __name__ == "__main__":
+    for motor in [left_motor, right_motor]:
+        motor.setPosition(float('inf'))
+        motor.setVelocity(0.0)
     
-    while robot.step(timestep) != -1:
-        limit = 500
+    for ir in [left_ir, straight_ir, right_ir, top_ir]:
+        ir.enable(timestep)
+    
+    while robot.step(timestep) != -1 and counter:
         straight = straight_ir.getValue()>limit
         left = left_ir.getValue()>limit
         right = right_ir.getValue()>limit
+
         top = top_ir.getValue()==1000
-        if not flag:
-            if top and left and right and straight:
-                flag = True
-                left_speed= max_speed
-                right_speed = max_speed
-            elif left:
-                left_speed = 0*max_speed
-                right_speed = max_speed
-                print("Left")
-            elif straight:
-                left_speed = max_speed
-                right_speed = max_speed
-                print("Straight")
-            elif right:
-                left_speed = max_speed
-                right_speed = 0*max_speed
-                print("Right")
-            else:
-                left_speed = -max_speed
-                right_speed = max_speed
-                print("Back")
 
-        print(f'Left: {left_ir.getValue()} Straight: {straight_ir.getValue()} Right: {right_ir.getValue()} Top: {top_ir.getValue()}')
+        if top and left and right and straight:
+            flag = True
+
+        if flag:
+            dispatch(state, { 'type': ['S', 'STOP'][counter==1] })
+        elif left:
+            dispatch(state, { 'type': 'L' })
+        elif straight:
+            dispatch(state, { 'type': 'S' })
+        elif right:
+            dispatch(state, { 'type': 'R' })
+        else:
+            dispatch(state, { 'type': 'B' })
+
+        if counter != 0: print(f'Left: {left_ir.getValue()} Straight: {straight_ir.getValue()} Right: {right_ir.getValue()} Top: {top_ir.getValue()}')
         
-        if not flag or c:
-            left_motor.setVelocity(left_speed)
-            right_motor.setVelocity(right_speed)
-            if flag: 
-                c -= 1 
-                if c==0: 
-                    left_motor.setVelocity(0)
-                    right_motor.setVelocity(0)
+        left_motor.setVelocity(state['left_speed'])
+        right_motor.setVelocity(state['right_speed'])
 
+        if flag: counter -= 1
